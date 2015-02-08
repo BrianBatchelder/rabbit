c2814fbb09da4d9b5bd9516d84b4591fb672997a
Brian Batchelder <brian@briansbrain.biz>
2015-02-08 11:35:57 -0800

    Calculate pace and update the pace text field. NOTE: this never worked as well as we would like. We hypothesized that the error factor in the GPS coordinate was overwhelming our calculation.  Perhaps using more data points, or more time between data points, or somehow taking the error into account would help.

--- movement/ViewController.m
+++ movement/ViewController.m
@@ -11,11 +11,15 @@
 @interface ViewController ()
 
 @property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) NSMutableArray *previousLocations;
+@property (nonatomic, strong) CLLocation *firstLocation;
 @end
 
 @implementation ViewController
 
 @synthesize locationManager = _locationManager;
+@synthesize previousLocations = _previousLocations;
+
 - (void)startStandardUpdates
 {
     // Create the location manager if this object does not
@@ -35,10 +39,75 @@
     [_locationManager startUpdatingLocation];
 }
 
+#define METERS_PER_MILE 1609.34
+#define MINUTES_PER_HOUR 60
+#define SECONDS_PER_MINUTE 60
+#define SECONDS_PER_HOUR (MINUTES_PER_HOUR * SECONDS_PER_MINUTE)
+#define TIME_DELTA_FOR_LEVELING_IN_SECONDS 20
+
+// Delegate method from the CL_locationManagerDelegate protocol.
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation* location = [locations lastObject];
+    
+    NSLog(@"");
+    if ([_previousLocations count] == 0) {
+        [_previousLocations addObject:location];
+    } else {
+        BOOL cleanedLocations = NO;
+        CLLocation *_oldestLocation = nil;
+        while (!cleanedLocations) {
+            _oldestLocation = [_previousLocations objectAtIndex:0];
+            NSTimeInterval timeDeltaInSeconds = [location.timestamp timeIntervalSinceDate:_oldestLocation.timestamp];
+            if (timeDeltaInSeconds > TIME_DELTA_FOR_LEVELING_IN_SECONDS) {
+                [_previousLocations removeObjectAtIndex:0];
+            } else {
+                cleanedLocations = YES;
+            }
+        }
+        [_previousLocations addObject:location];
+        
+        NSTimeInterval timeDeltaInSeconds = [location.timestamp timeIntervalSinceDate:_oldestLocation.timestamp];
+        
+        CLLocation *lastLocation = nil;
+        double distanceInMeters = 0;
+        for (CLLocation *thisLocation in _previousLocations) {
+            if (lastLocation) {
+                distanceInMeters += [thisLocation distanceFromLocation:lastLocation];
+            }
+            lastLocation = thisLocation;
+        }
+        
+        NSLog(@"distance (meters) = %f",distanceInMeters);
+        if (distanceInMeters > 0) {
+            
+            NSLog(@"time delta (seconds) = %f",timeDeltaInSeconds);
+            double speedInMetersPerSecond = distanceInMeters / timeDeltaInSeconds;
+            NSLog(@"speed %+.6f meters/second\n", speedInMetersPerSecond);
+            if (speedInMetersPerSecond <= 0) {
+                _pace.text = @"Not moving";
+            } else {
+                double milesPerHour = (speedInMetersPerSecond/METERS_PER_MILE*SECONDS_PER_HOUR);
+                NSLog(@"speed %+.6f miles/hour\n", milesPerHour);
+                double minutesPerMile = MINUTES_PER_HOUR / milesPerHour;
+                NSLog(@"pace %+.6f minutes/miles\n", minutesPerMile);
+                
+                double minutes;
+                double fractPart = modf(minutesPerMile,&minutes);
+                double seconds = fractPart*SECONDS_PER_MINUTE;
+                _pace.text = [NSString stringWithFormat:@"%2.0f:%02.0f",minutes,seconds];
+            }
+        } else {
+            _pace.text = @"Not moving";
+        }
+    }
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     _pace.text = @"7:00";
+    _previousLocations = [[NSMutableArray alloc] init];
+    
     [self startStandardUpdates];
 }
 
