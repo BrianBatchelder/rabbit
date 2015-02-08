3ebc4de0d57eee62f6430412327e4f40d7e64aec
Brian Batchelder <brian@briansbrain.biz>
2015-02-08 11:23:09 -0800

    Add basic Core Location code.

--- movement/ViewController.m
+++ movement/ViewController.m
@@ -10,13 +10,35 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) CLLocationManager *locationManager;
 @end
 
 @implementation ViewController
 
+@synthesize locationManager = _locationManager;
+- (void)startStandardUpdates
+{
+    // Create the location manager if this object does not
+    // already have one.
+    if (nil == _locationManager) {
+        _locationManager = [[CLLocationManager alloc] init];
+    }
+    
+    [_locationManager requestAlwaysAuthorization];
+    
+    _locationManager.delegate = self;
+    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    // Set a movement threshold for new events.
+    //_locationManager.distanceFilter = 500; // meters
+    
+    [_locationManager startUpdatingLocation];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self startStandardUpdates];
 }
 
 - (void)didReceiveMemoryWarning {
