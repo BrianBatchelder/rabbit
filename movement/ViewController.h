c43ce8612910f69779e08757a1d371e62ccefca4
Brian Batchelder <brian@briansbrain.biz>
2015-02-08 11:24:10 -0800

    Fix previous commit.

--- movement/ViewController.h
+++ movement/ViewController.h
@@ -9,7 +9,6 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
 @interface ViewController : UIViewController <CLLocationManagerDelegate>
 
 
